cb2cf19f289c84ac31e73baf38f8641ef964d421
quacoo <quacoo.boat@gmail.com>
2025-06-01 07:13:59 +0000

    ✨ Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
+
 <template>
   <header>
     <AppNavbar />
   </header>
-  <main class="mt-16">
+  <main>
     <UContainer>
       <slot />
     </UContainer>
   </main>
+  <footer class="fixed bottom-0 inset-x-0 p-2 text-muted text-sm text-right">
+    <UContainer>
+      Tracking your fitness💪🏾 &copy; {{ currentYear }}
+    </UContainer>
+  </footer>
 </template>
