47adef5419f3cdb0f3ddace5b1c244e19a8f35bd
quacoo <quacoo.boat@gmail.com>
2025-06-01 07:00:59 +0000

    ✨ Add app menu

--- components/app/navbar.vue
+++ components/app/navbar.vue
@@ -20,12 +20,12 @@ async function logout() {
 <template>
   <nav class="bg-default border-b border-muted">
     <UContainer>
-      <div class="relative flex h-16 items-center justify-between">
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex shrink-0 items-center">
-            <UButton size="xl" variant="ghost" color="neutral" icon="hugeicons:equipment-gym-03" :ui="{ base: 'font-mono', leadingIcon: 'text-primary' }">
-              Fitness
-            </UButton>
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex space-x-2 ">
+          <AppSidebar v-if="user" />
+          <div class="flex items-center space-x-2">
+            <Icon name="hugeicons:equipment-gym-03" class="text-primary size-6" />
+            <span class="font-mono">Fitness</span>
           </div>
         </div>
         <div class="flex space-x-4">
