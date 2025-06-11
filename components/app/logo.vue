f64ba85d2e0d06b13be165b6f040968f8ffd41a8
quacoo <quacoo.boat@gmail.com>
2025-06-11 20:53:33 +0000

    💄  add sans and logo font

---
+++ components/app/logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center gap-1">
+    <UIcon name="hugeicons:equipment-gym-03" class="text-primary size-6" />
+    <span class="font-logo leading-none mt-1 text-2xl">Fitness</span>
+  </div>
+</template>
